8ec2ee482771a686818d472116ed6d3266b275a4
Luan Henrique <luan@yepcomm.com.br>
2023-02-13 11:04:21 -0300

    feat: Tabs Ask > arrows

--- src/components/TabsAsk/index.js
+++ src/components/TabsAsk/index.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import './styles.css';
+
 function TabsAsk({ title, lastItem = false }) {
 
   const [active, setActive] = useState(false);
@@ -8,14 +10,20 @@ function TabsAsk({ title, lastItem = false }) {
     <li>
       <p
         onClick={() => active ? setActive(false) : setActive(true)}
-        className={`${lastItem && !active ? 'border-y' : 'border-t'} py-3 border-t border-[#dedede] cursor-pointer`}
+        className={
+          `${lastItem && !active ? 'border-y' : 'border-t'}
+          py-3 border-t border-[#dedede] cursor-pointer flex items-center justify-between gap-4 pr-4 arrow-down
+          ${active ? 'active' : ''}
+          `}
       >
         {title}
       </p>
       <div className={`${active ? 'block' : 'hidden'} text-gray font-normal pb-3`}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non maiores veritatis,
-        cum aperiam fugit ex amet incidunt mollitia laudantium assumenda quas blanditiis,
-        culpa porro excepturi, aliquam consequatur ratione est. Placeat.
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non maiores veritatis,
+          cum aperiam fugit ex amet incidunt mollitia laudantium assumenda quas blanditiis,
+          culpa porro excepturi, aliquam consequatur ratione est. Placeat.
+        </p>
       </div>
     </li>
   );
