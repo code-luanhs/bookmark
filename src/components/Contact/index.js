2e01fb18e807deba648bdf6efd41966caf721bc1
Luan Henrique <luan@yepcomm.com.br>
2023-02-13 18:07:52 -0300

    feat: validacao form de contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,10 @@
+import { useForm } from "react-hook-form";
+
 function Contact() {
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
   return (
     <section className="bg-soft-blue mt-16 max-lg:px-8">
       <div className="max-w-container mx-auto py-16 text-center text-white">
@@ -8,19 +14,29 @@ function Contact() {
         </p>
         <h2 className="text-h2 lg:max-w-[400px] mx-auto my-7">Stay up-to-date with what we're doing</h2>
 
-        <form className="flex items-center justify-center gap-3 max-lg:flex-col">
-          <input
-            type="name"
-            placeholder="Enter your email address"
-            className="bg-white rounded py-2.5 px-4 w-full max-w-[330px] text-dark-blue placeholder:text-[#ccc] font-normal max-lg:max-w-[100%]"
-            required
-          />
+        <form onSubmit={handleSubmit(onSubmit)} className="flex items-start justify-center gap-3 max-lg:flex-col">
+          <div className="relative w-full lg:max-w-[330px]">
+            <input
+              {...register("email", {required: true})}
+              placeholder="Enter your email address"
+              className={`
+                bg-white rounded py-2.5 px-4 w-full text-dark-blue placeholder:text-[#ccc]
+                font-normal max-lg:max-w-[100%] ${errors.email ? 'border-2 border-soft-red outline-none' : ''}
+              `}
+            />
+            {errors.email && <img src="images/icon-error.svg" alt="Ícone de erro" loading="lazy" className="absolute right-3 top-3" />}
+            {errors.email && <p className="text-[.75rem] text-left bg-soft-red rounded-b py-1 px-2 relative bottom-[2px]">Whoops, make sure it's an email</p>}
+          </div>
+
           <button
-            onClick={(e) => e.preventDefault()}
-            className="bg-soft-red rounded py-2 px-5 border-2 border-soft-red duration-300 hover:bg-white hover:text-soft-red max-lg:w-full"
+            type="submit"
+            className={`
+              bg-soft-red rounded py-2 px-5 border-2 border-soft-red duration-300
+              hover:bg-white hover:text-soft-red max-lg:w-full
+            `}
           >
             Contact Us
-            </button>
+          </button>
         </form>
       </div>
     </section>
